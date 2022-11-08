e779c4eef5fde40d8a27b1ff73b6e061e52e74c5
muzahid37 <muzahidul372709@gmail.com>
2022-11-08 23:07:40 +0600

    add game over

--- Snake.js
+++ Snake.js
@@ -15,7 +15,10 @@ var speedY = 0;
 //food style
 var foodX;
 var foody;
+var gameOver = false;
 
+// snake body
+var snakeBody = [];
 window.onload = function () {
   box = document.getElementById("box");
   box.height = rows * blockSize;
@@ -24,9 +27,12 @@ window.onload = function () {
   foodPosition();
   document.addEventListener("keyup", ChangeDrection);
   //   update();
-  setInterval(update, 1000 / 10);
+  setInterval(update, 1000 / 5);
 };
 function update() {
+  if (gameOver) {
+    return;
+  }
   context.fillStyle = "black";
   context.fillRect(0, 0, box.width, box.height);
 
@@ -34,26 +40,51 @@ function update() {
   sankeX += speedX * blockSize;
   snakeY += speedY * blockSize;
   context.fillRect(sankeX, snakeY, blockSize, blockSize);
+  for (let i = 0; i < snakeBody.length; i++) {
+    context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+  }
   // eat food
   if (sankeX == foodX && snakeY == foody) {
+    snakeBody.push([foodX, foody]);
     foodPosition();
   }
-
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+  if (snakeBody.length) {
+    snakeBody[0] = [sankeX, snakeY];
+  }
   context.fillStyle = "red";
   context.fillRect(foodX, foody, blockSize, blockSize);
+
+  if (
+    sankeX < 0 ||
+    sankeX > collams * blockSize ||
+    snakeY < 0 ||
+    snakeY > rows * blockSize
+  ) {
+    gameOver = true;
+    alert("game ovar");
+  }
+  for (let i = 0; i < snakeBody.length; i++) {
+    if (sankeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+      gameOver = true;
+      alert("game over");
+    }
+  }
 }
 
 function ChangeDrection(e) {
-  if (e.code == "ArrowUp") {
+  if (e.code == "ArrowUp" && speedY != 1) {
     speedX = 0;
     speedY = -1;
-  } else if (e.code == "ArrowDown") {
+  } else if (e.code == "ArrowDown" && speedY != -1) {
     speedX = 0;
     speedY = 1;
-  } else if (e.code == "ArrowLeft") {
+  } else if (e.code == "ArrowLeft" && speedX != 1) {
     speedX = -1;
     speedY = 0;
-  } else if (e.code == "ArrowRight") {
+  } else if (e.code == "ArrowRight" && speedX != -1) {
     speedX = 1;
     speedY = 0;
   }
